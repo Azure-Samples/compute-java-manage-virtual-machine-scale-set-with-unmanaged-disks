d5792a652175823713a5428b0b2f32e3d02fed86
Azure-Fluent <azfluent@microsoft.com>
2020-10-26 13:07:28 +0800

    Release azure-resourcemanager 2.0.0

--- src/main/java/com/azure/resourcemanager/compute/samples/ManageVirtualMachineScaleSetWithUnmanagedDisks.java
+++ src/main/java/com/azure/resourcemanager/compute/samples/ManageVirtualMachineScaleSetWithUnmanagedDisks.java
@@ -1,30 +1,29 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-package com.microsoft.azure.management.compute.samples;
+package com.azure.resourcemanager.compute.samples;
 
-import com.microsoft.azure.PagedList;
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.compute.ImageReference;
-import com.microsoft.azure.management.compute.VirtualMachineScaleSetVM;
-import com.microsoft.azure.management.network.LoadBalancerInboundNatRule;
-import com.microsoft.azure.management.network.Network;
-import com.microsoft.azure.management.network.PublicIPAddress;
-import com.microsoft.azure.management.network.LoadBalancer;
-import com.microsoft.azure.management.network.TransportProtocol;
-import com.microsoft.azure.management.compute.VirtualMachineScaleSet;
-import com.microsoft.azure.management.compute.VirtualMachineScaleSetSkuTypes;
-import com.microsoft.azure.management.network.VirtualMachineScaleSetNetworkInterface;
-import com.microsoft.azure.management.network.VirtualMachineScaleSetNicIPConfiguration;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
-import com.microsoft.azure.management.resources.fluentcore.utils.SdkContext;
-import com.microsoft.azure.management.samples.Utils;
-import com.microsoft.rest.LogLevel;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.compute.models.ImageReference;
+import com.azure.resourcemanager.compute.models.VirtualMachineScaleSetVM;
+import com.azure.resourcemanager.network.models.LoadBalancerInboundNatRule;
+import com.azure.resourcemanager.network.models.Network;
+import com.azure.resourcemanager.network.models.PublicIpAddress;
+import com.azure.resourcemanager.network.models.LoadBalancer;
+import com.azure.resourcemanager.network.models.TransportProtocol;
+import com.azure.resourcemanager.compute.models.VirtualMachineScaleSet;
+import com.azure.resourcemanager.compute.models.VirtualMachineScaleSetSkuTypes;
+import com.azure.resourcemanager.network.models.VirtualMachineScaleSetNetworkInterface;
+import com.azure.resourcemanager.network.models.VirtualMachineScaleSetNicIpConfiguration;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.resourcemanager.samples.Utils;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -45,14 +44,14 @@ public final class ManageVirtualMachineScaleSetWithUnmanagedDisks {
 
     /**
      * Main function which runs the actual sample.
-     * @param azure instance of the azure client
+     * @param azureResourceManager instance of the azure client
      * @return true if sample runs successfully
      */
-    public static boolean runSample(Azure azure) {
+    public static boolean runSample(AzureResourceManager azureResourceManager) {
         final Region region = Region.US_EAST2;
-        final String rgName = SdkContext.randomResourceName("rgCOVS", 15);
-        final String vnetName = SdkContext.randomResourceName("vnet", 24);
-        final String loadBalancerName1 = SdkContext.randomResourceName("intlb" + "-", 18);
+        final String rgName = Utils.randomResourceName(azureResourceManager, "rgCOVS", 15);
+        final String vnetName = Utils.randomResourceName(azureResourceManager, "vnet", 24);
+        final String loadBalancerName1 = Utils.randomResourceName(azureResourceManager, "intlb" + "-", 18);
         final String publicIpName = "pip-" + loadBalancerName1;
         final String frontendName = loadBalancerName1 + "-FE1";
         final String backendPoolName1 = loadBalancerName1 + "-BAP1";
@@ -65,10 +64,10 @@ public final class ManageVirtualMachineScaleSetWithUnmanagedDisks {
         final String natPool50XXto22 = "natPool50XXto22";
         final String natPool60XXto23 = "natPool60XXto23";
 
-        final String vmssName =  SdkContext.randomResourceName("vmss", 24);
-        final String storageAccountName1 = SdkContext.randomResourceName("stg1", 24);
-        final String storageAccountName2 = SdkContext.randomResourceName("stg2", 24);
-        final String storageAccountName3 = SdkContext.randomResourceName("stg3", 24);
+        final String vmssName =  Utils.randomResourceName(azureResourceManager, "vmss", 24);
+        final String storageAccountName1 = Utils.randomResourceName(azureResourceManager, "stg1", 24);
+        final String storageAccountName2 = Utils.randomResourceName(azureResourceManager, "stg2", 24);
+        final String storageAccountName3 = Utils.randomResourceName(azureResourceManager, "stg3", 24);
 
         final String userName = "tirekicker";
         final String sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfSPC2K7LZcFKEO+/t3dzmQYtrJFZNxOsbVgOVKietqHyvmYGHEC0J2wPdAqQ/63g/hhAEFRoyehM+rbeDri4txB3YFfnOK58jqdkyXzupWqXzOrlKY4Wz9SKjjN765+dqUITjKRIaAip1Ri137szRg71WnrmdP3SphTRlCx1Bk2nXqWPsclbRDCiZeF8QOTi4JqbmJyK5+0UqhqYRduun8ylAwKKQJ1NJt85sYIHn9f1Rfr6Tq2zS0wZ7DHbZL+zB5rSlAr8QyUdg/GQD+cmSs6LvPJKL78d6hMGk84ARtFo4A79ovwX/Fj01znDQkU6nJildfkaolH2rWFG/qttD azjava@javalib.com";
@@ -82,7 +81,7 @@ public final class ManageVirtualMachineScaleSetWithUnmanagedDisks {
             // Create a virtual network with a frontend subnet
             System.out.println("Creating virtual network with a frontend subnet ...");
 
-            Network network = azure.networks().define(vnetName)
+            Network network = azureResourceManager.networks().define(vnetName)
                     .withRegion(region)
                     .withNewResourceGroup(rgName)
                     .withAddressSpace("172.16.0.0/16")
@@ -99,7 +98,7 @@ public final class ManageVirtualMachineScaleSetWithUnmanagedDisks {
             // Create a public IP address
             System.out.println("Creating a public IP address...");
 
-            PublicIPAddress publicIPAddress = azure.publicIPAddresses().define(publicIpName)
+            PublicIpAddress publicIPAddress = azureResourceManager.publicIpAddresses().define(publicIpName)
                     .withRegion(region)
                     .withExistingResourceGroup(rgName)
                     .withLeafDomainLabel(publicIpName)
@@ -134,7 +133,7 @@ public final class ManageVirtualMachineScaleSetWithUnmanagedDisks {
                     + "  balancer to a port for a specific virtual machine in the backend address pool\n"
                     + "  - this provides direct VM connectivity for SSH to port 22 and TELNET to port 23");
 
-            LoadBalancer loadBalancer1 = azure.loadBalancers().define(loadBalancerName1)
+            LoadBalancer loadBalancer1 = azureResourceManager.loadBalancers().define(loadBalancerName1)
                     .withRegion(region)
                     .withExistingResourceGroup(rgName)
 
@@ -171,7 +170,7 @@ public final class ManageVirtualMachineScaleSetWithUnmanagedDisks {
 
                     // Explicitly define the frontend
                     .definePublicFrontend(frontendName)
-                        .withExistingPublicIPAddress(publicIPAddress)
+                        .withExistingPublicIpAddress(publicIPAddress)
                         .attach()
 
                     // Add two probes one per rule
@@ -206,7 +205,7 @@ public final class ManageVirtualMachineScaleSetWithUnmanagedDisks {
                     .withSku("18.04-LTS")
                     .withVersion("latest");
 
-            VirtualMachineScaleSet virtualMachineScaleSet = azure.virtualMachineScaleSets().define(vmssName)
+            VirtualMachineScaleSet virtualMachineScaleSet = azureResourceManager.virtualMachineScaleSets().define(vmssName)
                     .withRegion(region)
                     .withExistingResourceGroup(rgName)
                     .withSku(VirtualMachineScaleSetSkuTypes.STANDARD_D3_V2)
@@ -241,13 +240,13 @@ public final class ManageVirtualMachineScaleSetWithUnmanagedDisks {
             System.out.println();
 
             // Print virtual machine scale set details
-            // Utils.print(virtualMachineScaleSet);
+            // ResourceManagerUtils.print(virtualMachineScaleSet);
 
             //=============================================================
             // List virtual machine scale set network interfaces
 
             System.out.println("Listing scale set network interfaces ...");
-            PagedList<VirtualMachineScaleSetNetworkInterface> vmssNics = virtualMachineScaleSet.listNetworkInterfaces();
+            PagedIterable<VirtualMachineScaleSetNetworkInterface> vmssNics = virtualMachineScaleSet.listNetworkInterfaces();
             for (VirtualMachineScaleSetNetworkInterface vmssNic : vmssNics) {
                 System.out.println(vmssNic.id());
             }
@@ -259,10 +258,10 @@ public final class ManageVirtualMachineScaleSetWithUnmanagedDisks {
             for (VirtualMachineScaleSetVM instance : virtualMachineScaleSet.virtualMachines().list()) {
                 System.out.println("Scale set virtual machine instance #" + instance.instanceId());
                 System.out.println(instance.id());
-                PagedList<VirtualMachineScaleSetNetworkInterface> networkInterfaces = instance.listNetworkInterfaces();
+                PagedIterable<VirtualMachineScaleSetNetworkInterface> networkInterfaces = instance.listNetworkInterfaces();
                 // Pick the first NIC
-                VirtualMachineScaleSetNetworkInterface networkInterface = networkInterfaces.get(0);
-                for (VirtualMachineScaleSetNicIPConfiguration ipConfig :networkInterface.ipConfigurations().values()) {
+                VirtualMachineScaleSetNetworkInterface networkInterface = networkInterfaces.iterator().next();
+                for (VirtualMachineScaleSetNicIpConfiguration ipConfig :networkInterface.ipConfigurations().values()) {
                     if (ipConfig.isPrimary()) {
                         List<LoadBalancerInboundNatRule> natRules = ipConfig.listAssociatedLoadBalancerInboundNatRules();
                         for (LoadBalancerInboundNatRule natRule : natRules) {
@@ -315,15 +314,10 @@ public final class ManageVirtualMachineScaleSetWithUnmanagedDisks {
             System.out.println("re-started virtual machine scale set");
 
             return true;
-        } catch (Exception f) {
-
-            System.out.println(f.getMessage());
-            f.printStackTrace();
-
         } finally {
             try {
                 System.out.println("Deleting Resource Group: " + rgName);
-                azure.resourceGroups().deleteByName(rgName);
+                azureResourceManager.resourceGroups().beginDeleteByName(rgName);
                 System.out.println("Deleted Resource Group: " + rgName);
             } catch (NullPointerException npe) {
                 System.out.println("Did not create any resources in Azure. No clean up is necessary");
@@ -331,7 +325,6 @@ public final class ManageVirtualMachineScaleSetWithUnmanagedDisks {
                 g.printStackTrace();
             }
         }
-        return false;
     }
 
     /**
@@ -345,18 +338,21 @@ public final class ManageVirtualMachineScaleSetWithUnmanagedDisks {
             //=============================================================
             // Authenticate
 
-            System.out.println(System.getenv("AZURE_AUTH_LOCATION"));
-            final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+            final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                .build();
 
-            Azure azure = Azure.configure()
-                    .withLogLevel(LogLevel.BASIC)
-                    .authenticate(credFile)
-                    .withDefaultSubscription();
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                .configure()
+                .withLogLevel(HttpLogDetailLevel.BASIC)
+                .authenticate(credential, profile)
+                .withDefaultSubscription();
 
             // Print selected subscription
-            System.out.println("Selected subscription: " + azure.subscriptionId());
+            System.out.println("Selected subscription: " + azureResourceManager.subscriptionId());
 
-            runSample(azure);
+            runSample(azureResourceManager);
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
